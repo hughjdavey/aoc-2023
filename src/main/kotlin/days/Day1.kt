9ce8d3ac7e1b027f2061c8c408d77c1f0b8b0ae4
hughjdavey <9026053+hughjdavey@users.noreply.github.com>
2023-12-01 11:21:24 +0000

    Day 1
    Add inputs, solutions and tests for day 1
    Change README and project name from those of the template project

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,44 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        return calibrationSum(inputList, REGEX_NUMBERS)
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        return calibrationSum(inputList, REGEX_NUMBER_WORDS)
+    }
+
+    fun calibrationSum(lines: List<String>, digitRegex: Regex): Int {
+        return lines.sumOf { line ->
+            val values = calibrationValues(line, digitRegex)
+            "${values.first()}${values.last()}".toInt()
+        }
+    }
+
+    fun calibrationValues(line: String, digitRegex: Regex): List<Int> {
+        val matches = digitRegex.findAllOverlapping(line).map { it.value }
+        return matches.map { digit ->
+            val wordIndex = NUMBER_WORDS.indexOf(digit)
+            if (wordIndex == -1) digit.toInt() else wordIndex + 1
+        }
+    }
+
+    private fun Regex.findAllOverlapping(input: CharSequence): List<MatchResult> {
+        val matches = mutableListOf<MatchResult>()
+        var match = this.find(input)
+        while (match != null) {
+            matches.add(match)
+            match = this.find(input, match.range.first + 1)
+        }
+        return matches
+    }
+
+    companion object {
+
+        val REGEX_NUMBERS = Regex("\\d")
+
+        val REGEX_NUMBER_WORDS = Regex("\\d|one|two|three|four|five|six|seven|eight|nine")
+
+        val NUMBER_WORDS = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
     }
 }
--- src/test/kotlin/days/Day1Test.kt
+++ src/test/kotlin/days/Day1Test.kt
@@ -1,9 +1,8 @@
 package days
 
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.core.Is.`is`
-import org.hamcrest.core.IsInstanceOf.instanceOf
-import org.hamcrest.core.IsNull.notNullValue
+import org.hamcrest.Matchers.contains
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 
 class Day1Test {
@@ -12,14 +11,36 @@ class Day1Test {
 
     @Test
     fun testPartOne() {
-        assertThat(dayOne.partOne(), `is`("THIS IS"))
+        assertThat(dayOne.partOne(), `is`(142))
     }
 
     @Test
     fun testPartTwo() {
-        val partTwo = dayOne.partTwo()
-        assertThat(partTwo, notNullValue())
-        assertThat(partTwo, instanceOf(String::class.java))
-        assertThat(partTwo, `is`("FILE"))
+        // there is a different test input for part two so we can't call partTwo directly
+        assertThat(dayOne.calibrationSum(listOf(
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+        ), Day1.REGEX_NUMBER_WORDS), `is`(281))
+    }
+
+    @Test
+    fun testLineToDigit() {
+        assertThat(dayOne.calibrationValues("1abc2", Day1.REGEX_NUMBERS), contains(1, 2))
+        assertThat(dayOne.calibrationValues("pqr3stu8vwx", Day1.REGEX_NUMBERS), contains(3, 8))
+        assertThat(dayOne.calibrationValues("a1b2c3d4e5f", Day1.REGEX_NUMBERS), contains(1, 2, 3, 4, 5))
+        assertThat(dayOne.calibrationValues("treb7uchet", Day1.REGEX_NUMBERS), contains(7))
+
+        assertThat(dayOne.calibrationValues("two1nine", Day1.REGEX_NUMBER_WORDS), contains(2, 1, 9))
+        assertThat(dayOne.calibrationValues("eightwothree", Day1.REGEX_NUMBER_WORDS), contains(8, 2, 3))
+        assertThat(dayOne.calibrationValues("abcone2threexyz", Day1.REGEX_NUMBER_WORDS), contains(1, 2, 3))
+        assertThat(dayOne.calibrationValues("xtwone3four", Day1.REGEX_NUMBER_WORDS), contains(2, 1, 3, 4))
+        assertThat(dayOne.calibrationValues("4nineeightseven2", Day1.REGEX_NUMBER_WORDS), contains(4, 9, 8, 7, 2))
+        assertThat(dayOne.calibrationValues("zoneight234", Day1.REGEX_NUMBER_WORDS), contains(1, 8, 2, 3, 4))
+        assertThat(dayOne.calibrationValues("7pqrstsixteen", Day1.REGEX_NUMBER_WORDS), contains(7, 6))
     }
 }
